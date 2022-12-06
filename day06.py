7dfac16b929a520a50aa4638c4a908d6333036e7
slow-connect <113528799+slow-connect@users.noreply.github.com>
2022-12-06 11:17:49 +0100

    Day 06

---
+++ day06.py
@@ -0,0 +1,37 @@
+
+
+def fourdifferent(string):
+    for i in range(len(string)):
+        for j in range(len(string)):
+            if i != j:
+                if string[i] == string[j]:
+                    return False
+    return True
+
+def part1(input):
+    for i in range(len(input) - 4):
+        if fourdifferent(input[i:i+4]):
+            return i+4
+
+def part2(input):
+    for i in range(len(input) - 14):
+        if fourdifferent(input[i:i+14]):
+            return i+14
+
+
+input = open('day06_imput.txt', 'r')
+input_line = input.readline()
+input_line = input_line.replace('\n', '')
+while input_line != '':
+    print(part1(input_line))
+    input_line = input.readline()
+    input_line = input_line.replace('\n', '')
+
+
+input = open('day06_imput.txt', 'r')
+input_line = input.readline()
+input_line = input_line.replace('\n', '')
+while input_line != '':
+    print(part2(input_line))
+    input_line = input.readline()
+    input_line = input_line.replace('\n', '')
